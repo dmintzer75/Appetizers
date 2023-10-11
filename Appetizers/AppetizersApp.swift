2ae40a9ca9ea540f707766e15dcadf947d361656
Dario Mintzer <dmintzer75@hotmail.com>
2023-10-11 20:32:35 -0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by WKS-MAC01 on 11/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
