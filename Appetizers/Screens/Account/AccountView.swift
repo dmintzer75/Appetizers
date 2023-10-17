069031886ee58ae574f1f16a765cb2fe5df69d82
Dario Mintzer <dmintzer75@hotmail.com>
2023-10-16 21:37:12 -0300

    Finished Account section

--- Appetizers/Screens/Account/AccountView.swift
+++ Appetizers/Screens/Account/AccountView.swift
@@ -8,43 +8,43 @@
 import SwiftUI
 
 struct AccountView: View {
-    @State private var firstName = ""
-    @State private var lastName = ""
-    @State private var email = ""
-    @State private var birthdate = Date()
-    @State private var napkins = ""
-    @State private var extraNapkins = false
-    @State private var frequentRefills = false
+    @StateObject var viewModel = AccountViewModel()
     var body: some View {
         NavigationStack {
             Form {
                 Section("Personal Information") {
-                    TextField("First Name", text: $firstName)
+                    TextField("First Name", text: $viewModel.user.firstName)
 
-                    TextField("Last Name", text: $lastName)
+                    TextField("Last Name", text: $viewModel.user.lastName)
 
-                    TextField("Email", text: $email)
+                    TextField("Email", text: $viewModel.user.email)
                         .keyboardType(.emailAddress)
                         .textInputAutocapitalization(.never)
                         .autocorrectionDisabled()
 
-                    DatePicker("Birthday", selection: $birthdate, displayedComponents: .date)
+                    DatePicker("Birthday", selection: $viewModel.user.birthdate, displayedComponents: .date)
 
                     Button {
-                        print("Save")
+                        viewModel.saveChanges()
                     } label: {
                         Text("Save Changes")
                     }
                 }
 
                 Section("Requests") {
-                    Toggle("Extra Napkins", isOn: $extraNapkins)
+                    Toggle("Extra Napkins", isOn: $viewModel.user.extraNapkins)
                         .tint(.brandPrimary)
-                    Toggle("Frequent Refills", isOn: $frequentRefills)
+                    Toggle("Frequent Refills", isOn: $viewModel.user.frequentRefills)
                         .tint(.brandPrimary)
                 }
             }
             .navigationTitle("👨🏼‍💼 Account ")
+        }
+        .onAppear {
+            viewModel.retrieveUser()
+        }
+        .alert(item: $viewModel.alertItem) { alertItem in
+            Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
         }
     }
 }
--- Appetizers/Utilities/Alerts/Alert.swift
+++ Appetizers/Utilities/Alerts/Alert.swift
@@ -14,6 +14,8 @@ struct AlertItem: Identifiable {
 }
 
 enum AlertContext {
+    // MARK: - Network Alerts
+
     static let invalidData = AlertItem(title: Text("Server Error"),
                                        message: Text("The data received was invalid. Please contact support."),
                                        dismissButton: .default(Text("OK")))
@@ -26,4 +28,22 @@ enum AlertContext {
     static let unableToComplete = AlertItem(title: Text("Server Error"),
                                             message: Text("The data received was invalid. Please contact support."),
                                             dismissButton: .default(Text("OK")))
+
+    // MARK: - Account alerts
+
+    static let invalidForm = AlertItem(title: Text("Invalid Form"),
+                                       message: Text("Please make sure that all fields in the form have been filled out."),
+                                       dismissButton: .default(Text("OK")))
+
+    static let invalidEmail = AlertItem(title: Text("Invalid Email"),
+                                        message: Text("Please ensure your email is correct."),
+                                        dismissButton: .default(Text("OK")))
+
+    static let userSavedSuccess = AlertItem(title: Text("Profile Saved"),
+                                            message: Text("Your profile info was succesfully saved."),
+                                            dismissButton: .default(Text("OK")))
+
+    static let invalidUserData = AlertItem(title: Text("Profile error"),
+                                           message: Text("There was an error saving or retrieving your profile."),
+                                           dismissButton: .default(Text("OK")))
 }
