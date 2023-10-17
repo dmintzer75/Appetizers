0c4672282fb0afe593c8e68b3cf36b3f1b60f326
Dario Mintzer <dmintzer75@hotmail.com>
2023-10-16 21:40:17 -0300

    testing

---
+++ test.swift
@@ -0,0 +1,18 @@
+//
+//  test.swift
+//  Appetizers
+//
+//  Created by WKS-MAC01 on 16/10/2023.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    test()
+}
